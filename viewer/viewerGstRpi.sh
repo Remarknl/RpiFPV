6310d5afc1ecd33ebc3e94a28f34d6efbc0822f2
Richard Ginus <richardginus@gmail.com>
2014-08-12 00:50:57 +0200

    added rpi viewer

---
+++ viewer/viewerGstRpi.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 -v udpsrc port=5004 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtpjitterbuffer latency=5000 ! rtph264depay ! h264parse ! omxh264dec ! eglglessink
