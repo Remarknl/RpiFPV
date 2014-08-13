d712d9eb0fcf9644450bc85335315c9ee9c5d24c
Richard Ginus <richardginus@gmail.com>
2014-08-13 13:42:35 +0200

    Made scripts more readable and removed old viewer

--- viewer/viewerGst.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 -v udpsrc port=5004 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
