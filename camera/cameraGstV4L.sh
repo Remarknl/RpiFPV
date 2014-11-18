2f6850443d2f1b31a429b22c1fe92968c4d22acb
Richard Ginus <richardginus@gmail.com>
2014-11-18 13:42:15 +0100

    Added some more correct IP adresses

--- camera/cameraGstV4L.sh
+++ camera/cameraGstV4L.sh
@@ -3,4 +3,4 @@ gst-launch-1.0 -v v4l2src device=/dev/video0 \
 ! videoconvert \
 ! x264enc tune=zerolatency \
 ! rtph264pay config-interval=1 pt=96 \
-! udpsink host=videoserver.ulab.nl port=5004
+! udpsink host=192.168.1.3 port=5004
